524c4d275e8fbbf6656341af7395d67c677c989d
ltphat2204 <ltphat2204@gmail.com>
2024-04-28 21:51:51 +0700

    Add banner to home page

--- frontend/src/app/(shop)/page.tsx
+++ frontend/src/app/(shop)/page.tsx
@@ -1,6 +1,29 @@
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
+
 export default function Home() {
     return (
         <>
+            <div className={styles.bannerWrap}>
+                <video
+                    src="/banner.mp4"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    width={1920}
+                    height={1080}
+                    className={styles.bannerVideo}
+                />
+                <div className={styles.bannerDescriptionWrap}>
+                    <div className={styles.bannerDescriptionTitle}>VISION PRO</div>
+                    <div className={styles.bannerDescriptionSubTitle}>Khai phá giác quan, mở cảnh giới mới</div>
+                    <div className={styles.bannerDescriptionButtonWrap}>
+                        <Link className={`${styles.bannerDescriptionButton} ${styles.bannerDescriptionButtonOutline}`} href='/'>Tìm hiểu thêm</Link>
+                        <Link className={`${styles.bannerDescriptionButton} ${styles.bannerDescriptionButtonFill}`} href='/'>Mua ngay</Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
