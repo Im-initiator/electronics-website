32af3dcb4e417203ecb2dd12793669907ed4bf07
ltphat2204 <ltphat2204@gmail.com>
2024-04-28 21:12:17 +0700

    Add header component

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    cssModules: true,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: "[]",
+    },
+};
 
 export default nextConfig;
--- frontend/src/app/(shop)/layout.tsx
+++ frontend/src/app/(shop)/layout.tsx
@@ -1,8 +1,10 @@
+import { Header } from "@/components/Header";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <>
+            <Header/>
             {children}
         </>
     );
