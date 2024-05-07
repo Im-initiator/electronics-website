ab62c560d5bb696e5c8f3eafd75a73c5b9be5c6d
travelerrrrr <tienminhleminhss@gmail.com>
2024-05-08 00:40:02 +0700

    update container for docker

--- database/data.sql
+++ database/data.sql
@@ -1,1 +1,163 @@
---Init database here
+CREATE DATABASE  IF NOT EXISTS `electronics_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `electronics_project`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: electronics_project
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `create_date` datetime(6) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `modify_date` datetime(6) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `last_modify_by` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_q0uja26qgu1atulenwup9rxyr` (`email`),
+  UNIQUE KEY `UK_f6xpj7h12wr185bqhfi1hqlbr` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (1,NULL,1,NULL,NULL,NULL,'user@gmail.com',NULL,NULL,'$2a$10$zL2miL8Inx/kVsbLf3KaaOtEZNG/yho8JFD2V3uIrBHdP1l/Ulh.a','user');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `account_role`
+--
+
+DROP TABLE IF EXISTS `account_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account_role` (
+  `account_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  PRIMARY KEY (`account_id`,`role_id`),
+  KEY `FKrs2s3m3039h0xt8d5yhwbuyam` (`role_id`),
+  CONSTRAINT `FK1f8y4iy71kb1arff79s71j0dh` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `FKrs2s3m3039h0xt8d5yhwbuyam` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_role`
+--
+
+LOCK TABLES `account_role` WRITE;
+/*!40000 ALTER TABLE `account_role` DISABLE KEYS */;
+INSERT INTO `account_role` VALUES (1,1);
+/*!40000 ALTER TABLE `account_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `create_date` datetime(6) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `modify_date` datetime(6) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `last_modify_by` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (NULL,1,NULL,'USER',NULL,NULL,'user');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `token` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `create_date` datetime(6) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `last_modify_by` varchar(255) DEFAULT NULL,
+  `modify_date` datetime(6) DEFAULT NULL,
+  `access_token` varchar(255) NOT NULL,
+  `expired` tinyint(1) NOT NULL DEFAULT '0',
+  `refresh_token` varchar(255) NOT NULL,
+  `revoked` tinyint(1) NOT NULL DEFAULT '0',
+  `account_id` bigint DEFAULT NULL,
+  `token_type` enum('BEARER') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKftkstvcfb74ogw02bo5261kno` (`account_id`),
+  CONSTRAINT `FKftkstvcfb74ogw02bo5261kno` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `token`
+--
+
+LOCK TABLES `token` WRITE;
+/*!40000 ALTER TABLE `token` DISABLE KEYS */;
+INSERT INTO `token` VALUES (12,'2024-05-08 00:00:43.148799',NULL,NULL,'2024-05-08 00:00:43.148799','eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwic3ViIjoidXNlciIsImlhdCI6MTcxNTEwMTI0MywiZXhwIjoxNzE1MTAxMjQ0fQ.iRbPCCH4KaejrJWBLN-2Nzy0iazp7a01_oQJuFbRH4M',0,'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoiMSIsInN1YiI6InVzZXIiLCJpYXQiOjE3MTUxMDEyNDMsImV4cCI6MTcxNTcwNjA0M30.F3Knqy0_2hnVMwGkMkLeNjwZNqcdsiH4NX9AQfX9uVQ',0,1,NULL),(13,'2024-05-08 00:09:09.265202',NULL,NULL,'2024-05-08 00:09:09.265202','eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwic3ViIjoidXNlciIsImlhdCI6MTcxNTEwMTc0OSwiZXhwIjoxNzE1MTAxNzUwfQ.h1a1p6BG5-V6M3BzQdsbnNS6mJ469pFp2EHzQuLmtAY',0,'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoiMSIsInN1YiI6InVzZXIiLCJpYXQiOjE3MTUxMDE3NDksImV4cCI6MTcxNTEwMTc1MH0.OMNCY1AuAQTxtebT8a9Mtzwn59SPAagRpktf4yFyeKw',0,1,NULL),(14,'2024-05-08 00:13:44.063881',NULL,NULL,'2024-05-08 00:14:14.288744','eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwic3ViIjoidXNlciIsImlhdCI6MTcxNTEwMjA1NCwiZXhwIjoxNzE1MTg4NDU0fQ.u_-_C9f8ePRVDnTHuvNmd4XIOkR5uYAkJpLYE_6p8T8',0,'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoiMSIsInN1YiI6InVzZXIiLCJpYXQiOjE3MTUxMDIwMjMsImV4cCI6MTcxNTcwNjgyM30.iV5rpoNSC2SmNaDmO9bmKuTlJhEapmwb6q5LQdt-Vcg',0,1,NULL);
+/*!40000 ALTER TABLE `token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'electronics_project'
+--
+
+--
+-- Dumping routines for database 'electronics_project'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-08  0:39:13
